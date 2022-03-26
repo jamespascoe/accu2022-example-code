1d77243e475df0ff0332430b8bfdcba46ef53e05
James Pascoe <james.pascoe@bluwireless.com>
2022-03-26 12:33:56 +0000

    Format Code and Align Coroutine Names
    This commit aligns the names of the coroutines to match the Lua
    prototype. In addition, the code has been formatted with clang-format.

--- co_chat/co_chat.cpp
+++ co_chat/co_chat.cpp
@@ -2,7 +2,16 @@
 // co_chat.cpp
 // ~~~~~~~~~~~
 //
-// g++-11 -I/usr/local/boost_1_78_0/include -fcoroutines -std=c++20 -Wall -Werror -o co_chat co_chat.cpp -l pthread
+// Build (Linux Mint 19):
+//
+// g++-11 -I/usr/local/boost_1_78_0/include -fcoroutines -std=c++20
+// -Wall -Werror -o co_chat co_chat.cpp -l pthread
+//
+// Build (Mac OS Big Sur 11.6.5):
+//
+// g++-11 -I /usr/local/Cellar/boost/1.76.0/include
+// -L /usr/local/Cellar/boost/1.76.0/lib -fcoroutines -std=c++20 -Wall
+// -Werror -o co_chat co_chat.cpp -l pthread -l boost_regex
 //
 // terminal 1> ./co_chat 127.0.0.1 6666 127.0.0.1 7777
 // terminal 2> ./co_chat 127.0.0.1 7777 127.0.0.1 6666
@@ -21,48 +30,49 @@ using boost::asio::dynamic_buffer;
 using boost::asio::io_context;
 using boost::asio::steady_timer;
 using boost::asio::use_awaitable;
-using boost::asio::ip::tcp;
-using boost::asio::ip::basic_resolver_entry;
 using boost::asio::experimental::as_tuple;
+using boost::asio::ip::basic_resolver_entry;
+using boost::asio::ip::tcp;
+using boost::asio::ip::tcp::resolver::passive;
 
 using boost::system::error_code;
 
 class chat {
 
 public:
-
-  chat(char *addr, char *port, char *remote_addr, char *remote_port)
+  chat(char *addr, char *port, char *remote_addr,
+       char *remote_port)
       : remote_sock(ctx) {
 
     tcp::resolver resolver(ctx);
     basic_resolver_entry<tcp> listen_endpoint;
     basic_resolver_entry<tcp> remote_endpoint;
 
-    listen_endpoint = *resolver.resolve(addr, port, tcp::resolver::passive);
+    listen_endpoint = *resolver.resolve(addr, port, passive);
     remote_endpoint = *resolver.resolve(remote_addr, remote_port);
 
-    co_spawn(ctx, transmit(remote_endpoint), detached);
-    co_spawn(ctx, receive(listen_endpoint), detached);
+    co_spawn(ctx, sender(remote_endpoint), detached);
+    co_spawn(ctx, receiver(listen_endpoint), detached);
 
     ctx.run();
   }
 
 private:
-
-  awaitable<void> transmit(tcp::endpoint remote) {
+  awaitable<void> sender(tcp::endpoint remote) {
 
     for (;;) {
 
-      auto [error] =
-          co_await remote_sock.async_connect(remote, as_tuple(use_awaitable));
+      auto [error] = co_await remote_sock.async_connect(
+          remote, as_tuple(use_awaitable));
       if (!error) {
         std::cout << "Connected to: " << remote << std::endl;
         connected = true;
       } else {
-        std::cout << "Could not connect to: " << remote << " - retrying in 1s"
-                  << std::endl;
+        std::cout << "Could not connect to: " << remote
+                  << " - retrying in 1s" << std::endl;
 
-        steady_timer timer(co_await boost::asio::this_coro::executor);
+        steady_timer timer(
+            co_await boost::asio::this_coro::executor);
         timer.expires_after(std::chrono::seconds(1));
         co_await timer.async_wait(use_awaitable);
         continue;
@@ -71,7 +81,7 @@ private:
       std::string data;
       while (connected) {
         struct pollfd input[1] = {{.fd = 0, .events = POLLIN}};
-        if (poll(input, 1, 100)) {
+        if (poll(input, 1, 100 /* timeout in ms */)) {
           char c;
           while (std::cin.get(c) && c != '\n')
             data += c;
@@ -87,7 +97,7 @@ private:
     }
   }
 
-  awaitable<void> receive(tcp::endpoint listen) {
+  awaitable<void> receiver(tcp::endpoint listen) {
 
     tcp::acceptor acceptor(ctx, listen);
 
@@ -103,7 +113,8 @@ private:
               client, dynamic_buffer(str), boost::regex("\r\n"),
               as_tuple(use_awaitable));
 
-          if (error == boost::asio::error::eof) { // remote has disconnected
+          if (error ==
+              boost::asio::error::eof) { // remote has disconnected
             remote_sock.close();
             connected = false;
             break;
